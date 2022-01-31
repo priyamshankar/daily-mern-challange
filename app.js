8b8e4481e2da05d0408ef56d34965a77c3e07b30
priyam <priyamshankar.5@gmail.com>
2022-01-31 20:32:37 +0530

    adding another page using express

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 const express= require("express");
 const app=express();
+app.get('/',(req,res)=>{
+    res.send(`this is a home page`);
+});app.get('/notepad',(req,res)=>{
+    res.send(`this is a notepad page`);
+    // res.send(notetakingApp/index.html);
+});
+app.listen(80,()=>{
+    console.log("checking the listen command");
+});
