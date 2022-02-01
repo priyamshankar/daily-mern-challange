5330193d0ea4c77ff499c111644b2dafc3e53c2d
priyam <priyamshankar.5@gmail.com>
2022-02-01 23:06:33 +0530

    learnt basic command of mongodb and add mongolib

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-const express= require("express");
-const app=express();
-app.get('/',(req,res)=>{
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+app.get('/', (req, res) => {
     res.send(`this is a home page`);
-});app.get('/notepad',(req,res)=>{
+}); app.get('/notepad', (req, res) => {
     res.send(`this is a notepad page`);
     // res.send(notetakingApp/index.html);
 });
-app.listen(80,()=>{
+app.listen(80, () => {
     console.log("checking the listen command");
+    console.log("hii priyam the app is running");
 });
+mongoose.connect("mongodb://localhost:27017/dailymernchallange", { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log("connection successful").catch(() => console.log("connections failed")));
