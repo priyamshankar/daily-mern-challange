2165d499103674666c657aa20fff8fde8d047f57
priyam <priyamshankar.5@gmail.com>
2022-02-04 02:24:05 +0530

    conditions in mongo

--- app.js
+++ app.js
@@ -48,16 +48,16 @@ const schemaDb = new mongoose.model("schemaDb", addingSchema);
 
 
 // entering the values in the document
-const createDocument=async () => {
+const createDocument = async () => {
     try {
         const progressdocs = new schemaDb({
             name: "priyam",
             username: "priyamshankar",
             progress: true,
             playlistName: "mongodb tutorial thapa technical",
-            videoNo: 13,
-            topicCovered: "mongodb crud and through nodejs",
-            day: 3,
+            videoNo: 20,
+            topicCovered: "crud on node, inbuilt validation",
+            day: 4,
         });
         await progressdocs.save();      //use .insertMany([arrays of docs name ie progressdocs in this case]) instead of .save for manydocuments
         console.log("document created successfully");
@@ -68,11 +68,12 @@ const createDocument=async () => {
 }
 // createDocument();
 
-const getDocument= async()=>{
-    try{
-        const result=await schemaDb.find({progress:false }).select({videoNo:2,name:2});
+const getDocument = async () => {
+    try {
+        // const result=await schemaDb.find({progress:false }).select({videoNo:2,name:2});
+        const result = await schemaDb.find({ $and: [{ progress: true },{name:'priyam'}] });
         console.log(result);
-    } catch(error){
+    } catch (error) {
         console.log(error);
     }
 };
